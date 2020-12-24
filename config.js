19d9f56e28ec33b6ac492d582d6742dc2db70ac9
BigStar <pyrealcoder@hotmail.com>
2020-12-24 11:34:06 +0200

    delete credentials

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
   secret: 'worldisfullofdevelopers',
   dbconf : {
-    RDS_HOSTNAME : "unathiapp.cluster-chwffrfkntn0.us-east-1.rds.amazonaws.com",
-    RDS_USERNAME : "admin",
-    RDS_PASSWORD : "ooHANt33",
+    RDS_HOSTNAME : "",
+    RDS_USERNAME : "",
+    RDS_PASSWORD : "",
     RDS_PORT : 3306,
-    RDS_DATABASE_1 :"unathidb",
-    RDS_DATABASE_2 :"transactionsdb"
+    RDS_DATABASE_1 :"",
+    RDS_DATABASE_2 :""
   },
 };
